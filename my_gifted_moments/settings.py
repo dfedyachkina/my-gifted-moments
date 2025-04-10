98051a30480584b194787cca17f5126f9f40f837
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-04-10 17:38:01 +0000

    Add static in settings

--- my_gifted_moments/settings.py
+++ my_gifted_moments/settings.py
@@ -136,7 +136,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# STATIC_ROOT is only for production (remove or comment it out for dev)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
