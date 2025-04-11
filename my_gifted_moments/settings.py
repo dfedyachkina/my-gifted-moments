f730223753b7dde69102f82235cd99559ae459b1
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-04-11 06:46:26 +0000

    Add secret key in settings

--- my_gifted_moments/settings.py
+++ my_gifted_moments/settings.py
@@ -155,3 +155,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Secret Key
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
