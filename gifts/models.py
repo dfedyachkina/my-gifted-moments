2876df005134c8bf6b583bfb20458cc6c91f56e8
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-04-12 12:26:57 +0000

    Add models in gifts

---
+++ gifts/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from cloudinary.models import CloudinaryField
+
+
+class Occasion(models.Model):
+    """ Custom model for occasions: wedding, baby shower, etc. """
+    name = models.CharField(max_length=75, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    """ Custom model for categories: flowers, chocolates, etc. """
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Size(models.Model):
+    """ Custom model for sizes: small, medium, large, etc. """
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Gift(models.Model):
+    """ Custom model for gifts. """
+    name = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    occasion = models.ForeignKey(Occasion, on_delete=models.CASCADE, related_name="gifts_by_occasion")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="gifts_by_category")
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name="gifts_by_size")
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+    image = CloudinaryField("image", default="placeholder", null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.category.name}: {self.name}"
