779538dee3a1abee434fad976fd2b00df19a1716
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-04-12 12:24:11 +0000

    Hide secret key in settings

--- my_gifted_moments/settings.py
+++ my_gifted_moments/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-puocekb90ko6i=92l41kbdlf4de&nyoajjms$cb%cy_%xhzx8+"
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
