123a67040e6f60e1de68d482b7e0a03a2d6024ce
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-04-12 12:27:31 +0000

    Add admin.py in gifts

---
+++ gifts/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Occasion, Category, Size, Gift
+
+
+@admin.register(Occasion)
+class OccasionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Gift)
+class GiftAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'occasion', 'size', 'quantity', 'price', 'image')
+    list_filter = ('category', 'occasion', 'size')
+    search_fields = ('name', 'description')
