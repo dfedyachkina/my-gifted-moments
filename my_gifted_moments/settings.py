ccd5b9b9cf32e280bb6b3d30a349c9b9c69fae5a
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-04-12 12:25:20 +0000

    Add gifts url in project urls

--- my_gifted_moments/settings.py
+++ my_gifted_moments/settings.py
@@ -164,8 +164,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# Secret Key
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
 
 CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
--- my_gifted_moments/urls.py
+++ my_gifted_moments/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('home.urls')),
+    path('gifts/', include('gifts.urls')),
 ]
 
 if settings.DEBUG:
