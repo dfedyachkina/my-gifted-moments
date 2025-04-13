cf734448eaeb7a0a7a2f3ec1993bff44b394d30b
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-04-13 17:27:32 +0000

    Add friendly name to Category model in gifts

--- gifts/models.py
+++ gifts/models.py
@@ -11,11 +11,19 @@ class Occasion(models.Model):
 
 
 class Category(models.Model):
-    """ Custom model for categories: flowers, chocolates, etc. """
-    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
 
 
 class Size(models.Model):
