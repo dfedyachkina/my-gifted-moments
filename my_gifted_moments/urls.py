f7d1ce3aeed6d20fb1f26ebfc551fe9c9eba3045
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-04-14 18:41:16 +0000

    Add bag url to project's urls

--- my_gifted_moments/urls.py
+++ my_gifted_moments/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('home.urls')),
     path('gifts/', include('gifts.urls')),
+    path('bag/', include('bag.urls')),
 ]
 
 if settings.DEBUG:
