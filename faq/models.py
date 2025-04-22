a2b8bbc0a6941a5667c45fc8ddf387b7840e4894
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-04-22 12:17:17 +0000

    Create FAQ app and set up admin, urls, models and views

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class FAQ(models.Model):
+    question = models.CharField(null=False, blank=False, max_length=255)
+    answer = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.question
