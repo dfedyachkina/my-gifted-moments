75fd03629c0df659e50d48b50b2635d8019a078f
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-04-23 14:40:10 +0000

    Remove email backend line duplicate

--- my_gifted_moments/settings.py
+++ my_gifted_moments/settings.py
@@ -244,5 +244,4 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     ADMINS = os.environ.get('ADMINS')
