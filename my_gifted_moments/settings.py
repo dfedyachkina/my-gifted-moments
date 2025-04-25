c6a9a9c0edb8243ec0188aa9fed044d8e746b4a9
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-04-25 13:36:35 +0000

    Fix py errors validation in profiles

--- my_gifted_moments/settings.py
+++ my_gifted_moments/settings.py
@@ -207,7 +207,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -218,7 +217,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
 
 
 CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
