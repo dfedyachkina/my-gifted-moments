7d3a8ef1284898885701607025212a8dc1cb4518
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-04-27 14:02:47 +0000

    Fix duplicate email issue in newsletter

--- newsletter/models.py
+++ newsletter/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Newsletter(models.Model):
     """ Custom model to Newsletter. """
-    email = models.EmailField(null=False, blank=False)
+    email = models.EmailField(unique=True, null=False, blank=False)
 
     def __str__(self):
         return f"{self.email} subscribed on newsletter"
